e93789c8a0cbe3ac50ef8ce1cb9c7f27600fe4cc
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-03-28 16:27:17 -0400

    added a test api route

--- my-local-pro/src/app/api/hello/route.js
+++ my-local-pro/src/app/api/hello/route.js
@@ -1,3 +1,13 @@
+import { getUserById } from "@/lib/prisma/users"
+import { NextResponse } from "next/server";
+
 export async function GET(request) {
-  return new Response('Hello, Next.js!')
+  try {
+    const { foundUser, error } = await getUserById("641e862d653e36ae9b25af41");
+    if (error) throw new Error(error);
+    return NextResponse.json({ foundUser });
+  } catch (error) {
+    return NextResponse.json({ error: error.message});
+  }
+  // return new Response('Hello, Next.js!')
 }
